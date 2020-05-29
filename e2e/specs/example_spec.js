612ec433de00af83b135cb3b3b76c9e8d5d89ffe
Abhinaba Ghosh <askabhinaba@gmail.com>
2020-05-30 00:26:33 +0530

    updated wait logic

--- e2e/specs/example_spec.js
+++ e2e/specs/example_spec.js
@@ -1,12 +1,16 @@
 const until = protractor.ExpectedConditions;
 
-describe('Load page', function() {
-
-	it('Wait for react', async function() {
-		await browser.get('http://localhost:8080/');
-		await browser.waitForReact(10000, '#app');
-		await browser.wait(until.presenceOf(element(by.react('MyComponent')), 5000, 'Element taking too long to appear in the DOM'));
-		expect(element(by.react('MyComponent')).isPresent()).toBeTruthy();
-    });
-    
+describe('Load page', function () {
+  it('Wait for react', async function () {
+    await browser.get('http://localhost:8080/');
+    await browser.wait(
+      until.visibilityOf(
+        element(by.css('#app')),
+        5000,
+        'Element taking too long to appear in the DOM'
+      )
+    );
+    await browser.waitForReact(1000, '#app');
+    expect(element(by.react('MyComponent')).isPresent()).toBeTruthy();
+  });
 });
